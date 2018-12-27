7e347cf70bd3dc70b03fcbd2f480ca4658344b54
darren0609 <32918735+darren0609@users.noreply.github.com>
2018-12-27 14:17:04 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// Book is a list of book items to help store data associated with books.
+type Book struct {
+	Author string `xml:"author,attr"`
+	Title  string `xml:"title,attr"`
+	Year   string `xml:"hyr,attr"`
+	ID     string `xml:"owi,attr"`
+}
+
+func main() {
+	books, err := search("Harry Potter")
+	checkError(err)
+
+	fmt.Println(books)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func contentFromServer(url string) string {
+
+	resp, err := http.Get(url)
+	checkError(err)
+
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+
+	return string(bytes)
+}
+
+// ClassifySearchResponse is a struct for the results
+type ClassifySearchResponse struct {
+	Results []Book `xml:"works>work"`
+}
+
+func search(query string) ([]Book, error) {
+	var resp *http.Response
+	var err error
+
+	if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err != nil {
+		return []Book{}, err
+	}
+
+	defer resp.Body.Close()
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return []Book{}, err
+	}
+
+	var c ClassifySearchResponse
+	err = xml.Unmarshal(body, &c)
+
+	return c.Results, err
+}
